b5a68d5fe0349d1cbf76bf240f1f1f46b0184944
Nghia <nltnghia21@gmail.com>
2024-12-17 16:20:48 +0700

    fix bug api

--- models/Task.js
+++ models/Task.js
@@ -2,13 +2,14 @@ const mongoose = require('mongoose');
 
 const taskSchema = new mongoose.Schema(
     {
+        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true }, // Reference to User
         name: { type: String, required: true },
         description: { type: String },
         priority: { type: String, enum: ['High', 'Medium', 'Low'], required: true },
         estimatedTime: { type: Number }, // Time in hours
         status: { type: String, enum: ['Todo', 'In Progress', 'Completed', 'Expired'], default: 'Todo' },
-        startDate: { type: Date},
-        dueDate: { type: Date }, // Optional deadline or scheduling
+        startDate: { type: Date },
+        dueDate: { type: Date },
     },
     {
         timestamps: true,
